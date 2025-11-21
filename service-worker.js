9b340fb6819107beafc2044a7047d3236603b36a
4xxxmmv2s8-spec <4xxxmmv2s8@privaterelay.appleid.com>
2025-11-21 16:12:30 +0200

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'battery-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// התקנה – שמירת קבצים בקאש
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// ניקוי קאש ישן
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+// ניסיון לשרת מהקאש, ואם אין – מהאינטרנט
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
